683235c809ef45a6663b7e8549ef9c3bd662fbb5
Lucas Stil <lucas@platform.sh>
2019-06-21 20:38:05 +0200

    First library commit

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use serde_json::{json, Value};
+use std::env;
+
+pub fn is_valid_platform() -> bool {
+    let key = "APPLICATION_NAME";
+
+    match env::var(key) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+pub fn in_build() -> bool {
+    true
+}
+pub fn in_runtime() -> bool {
+    true
+}
+pub fn credentials(relationship: &str) -> Value {
+    let _ = relationship;
+    json!(null)
+}
+pub fn variable(name: &str) -> Result<&str, &str> {
+    let _ = name;
+    Ok("Ok")
+}
+pub fn variables() -> Value {
+    json!(null)
+}
+pub fn routes() -> Value {
+    json!(null)
+}
+pub fn get_route(id: &str) -> Value {
+    let _ = id;
+    json!(null)
+}
+pub fn application() -> Value {
+    json!(null)
+}
+pub fn on_enterprise() -> bool {
+    true
+}
+pub fn on_production() -> bool {
+    true
+}
+//pub fn registerFormatter(name: &str, callable $formatter) : self
+//pub fn formattedCredentials(string $relationship, string $formatter)
+pub fn has_relationship(relationship: &str) -> bool {
+    let _ = relationship;
+    true
+}
