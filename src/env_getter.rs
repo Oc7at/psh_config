31141825eb3067531c07836128c2dea137fcf42c
Lucas Stil <lucas@platform.sh>
2019-07-21 00:15:42 +0200

    Broken down a line with two `unwrap()`s in two in get_json_from_var

--- src/env_getter.rs
+++ src/env_getter.rs
@@ -17,5 +17,6 @@ pub fn get_env(var: &str) -> Option<String> {
 pub fn get_json_from_var(var: &str) -> Option<Value> {
     let b64_rel = get_env(var)?;
     let rel = base64::decode(&b64_rel).unwrap();
-    Some(serde_json::from_str(str::from_utf8(&rel).unwrap()).unwrap())
+    let rel_str = str::from_utf8(&rel).unwrap();
+    Some(serde_json::from_str(rel_str).unwrap())
 }
